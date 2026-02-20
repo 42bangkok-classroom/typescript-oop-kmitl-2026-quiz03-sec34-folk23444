fffd15548aee147a755192e60eebe65727fac2be
folk23444 <Thanyaton.jv@gmail.com>
2026-02-20 17:38:12 +0700

    Test

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,16 @@
-export class Car {}
+export class Car {
+brand: string = ''
+model: string = ''
+start(): void {
+    console.log("Toyota Camry is starting...")
+}
+accelerate(): void {
+    console.log("Toyota Camry is accelerating...")
+}
+brake(): void {
+    console.log("Toyota Camry is braking...")
+}
+stop(): void {
+    console.log("Toyota Camry is stopping...")
+}
+}
