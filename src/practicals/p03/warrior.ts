1d737618cb1dc1ccf0775e8bea1bffda8621210d
folk23444 <Thanyaton.jv@gmail.com>
2026-02-20 18:59:20 +0700

    Test9

--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,19 @@
 import { Character } from "./character";
 
 export class Warrior extends Character {
-  // TODO: implement class properties, constructor with super(...), and methods
+  weapon: string = "";
+
+  constructor(name: string, weapon: string, health: number = 100) {
+    super(name, health);
+    this.weapon = weapon;
+  }
+
+  getWeapon(): string {
+    return this.weapon;
+  }
+
+  receiveDamage(damage: number) {
+    const now = this.health;
+    this.health = now - (damage - damage * 0.1);
+  }
 }
