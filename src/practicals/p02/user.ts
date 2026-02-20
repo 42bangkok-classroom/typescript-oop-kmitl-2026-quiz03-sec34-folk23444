a7ee74e11c9f332464cc423dba943cdaf086ebc2
folk23444 <Thanyaton.jv@gmail.com>
2026-02-20 18:18:16 +0700

    Test5

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,11 +1,19 @@
 export class User {
-setFirstname: string = ""
-setLastname: string = ""
+Firstname: string = ""
+Lastname: string = ""
 
     private age: number = 0
 
     static BIRTH_YEAR: number = 2000
 
+    setFirstname(Firstname: string): void {
+        this.Firstname = Firstname
+    }
+
+    setLastname(Lastname: string): void {
+        this.Lastname = Lastname
+    }
+    
     setAge(age: number): void {
         this.age = age
     }
