97b2060d9730e14a061e6431d0a4287c8ca8a781
folk23444 <Thanyaton.jv@gmail.com>
2026-03-24 17:03:42 +0700

    1

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,16 +1,21 @@
-export class Car {
-brand: string = ''
-model: string = ''
-start(): void {
-    console.log(this.brand +" "+this.model +" "+"is starting...")
+export class car {
+brand = "Toyota"
+model = "Camry"    
+    constructor(brand:string,model:string){
+        this.brand = brand
+        this.model = model
+    }
+start():void {
+    console.log(`${this.brand} ${this.model} is starting...`)
 }
-accelerate(): void {
-    console.log(this.brand +" "+this.model +" "+"is accelerating...")
+accelerate():void{
+    console.log(`${this.brand} ${this.model} is accelerating...`)
 }
-brake(): void {
-    console.log(this.brand +" "+this.model +" "+"is braking...")
+brake():void{
+    console.log(`${this.brand} ${this.model} is braking...`)
 }
-stop(): void {
-    console.log(this.brand +" "+this.model +" "+"is stopping...")
+stop():void{
+    console.log(`${this.brand} ${this.model} is stopping...`)
 }
+
 }
